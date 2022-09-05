66ae68b743423af1d866eb17eda468eb5d045962
Roland Strasser <rol1510@users.noreply.github.com>
2022-09-06 00:35:05 +0200

    add preivew command

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::ffi::OsString;
 use std::fs;
 use std::path::PathBuf;
 
-use clap::{arg, Command};
+use clap::Command;
 use serde_derive::Deserialize;
 
 const CONFIG_FILE_PATH: &str = "./config.toml";
@@ -24,17 +24,8 @@ fn cli() -> Command<'static> {
         .subcommand_required(true)
         .arg_required_else_help(true)
         .allow_external_subcommands(true)
-        .subcommand(
-            Command::new("show").about("show the config file"), // .arg(arg!(<REMOTE> "The remote to clone"))
-                                                                // .arg(Arg::with_name())
-                                                                // .arg_required_else_help(true),
-        )
-        .subcommand(
-            Command::new("preview")
-                .about("prieview hat files are tracked")
-                .arg(arg!(<REMOTE> "The remote to clone"))
-                .arg_required_else_help(true),
-        )
+        .subcommand(Command::new("show").about("show the config file"))
+        .subcommand(Command::new("preview").about("show the config file"))
 }
 
 fn show() {
@@ -43,6 +34,34 @@ fn show() {
     let config = read_config();
 
     dbg!(config);
+}
+
+fn preview() {
+    let config = read_config();
+
+    config.units.into_iter().for_each(|item: Unit| {
+        println!("\nUnit {:?}", item.base);
+        let files = get_all_files(item.base);
+        println!("{:#?}", files);
+    });
+}
+
+fn get_all_files(path: PathBuf) -> Vec<PathBuf> {
+    let x = path.read_dir().expect("a");
+
+    let y = x.flat_map(|item| {
+        let p = item.unwrap().path();
+        let res: Vec<PathBuf> = if p.is_dir() {
+            get_all_files(p)
+        } else {
+            let mut v = Vec::new();
+            v.push(p);
+            return v;
+        };
+        res
+    });
+
+    return y.collect();
 }
 
 fn read_config() -> Config {
@@ -60,6 +79,9 @@ fn main() {
     match matches.subcommand() {
         Some(("show", _sub_matches)) => {
             show();
+        }
+        Some(("preview", _sub_matches)) => {
+            preview();
         }
         Some((ext, sub_matches)) => {
             let args = sub_matches
